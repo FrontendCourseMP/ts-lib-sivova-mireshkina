2a48c0b45a90a51d3874c095b19c347ce1526551
Dasha <lefward@yandex.ru>
2025-12-01 23:55:11 +0300

    мирешкина, немного расписала валидацию формы в main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 // Точка входа в ваше решение
+function form(form: HTMLFormElement) {
+  const inputs = form.querySelectorAll("input");
+  if (inputs.length === 0) {
+    throw new Error("нет инпутов!!");
+  }
+
+  for (const input of inputs) {
+    if (!input.labels || input.labels.length === 0) {
+      throw new Error("у инпута нет лэйбла");
+    }
+    if (!input.name) {
+      throw new Error("у инпута нет имени:(");
+    }
+    if (!input.placeholder) {
+      throw new Error("нет плэйсхолдера:(");
+    }
+    if (
+      !input.ariaDescribedByElements ||
+      input.ariaDescribedByElements.length === 0
+    ) {
+      throw new Error("нет поля для вывода ошибки");
+    }
+  }
+}
