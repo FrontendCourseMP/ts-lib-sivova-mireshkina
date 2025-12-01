21a9c93e37938ec5776d0f0dbf9e71427823dbca
sivova mariya <sivovamrv@gmail.com>
2025-12-02 00:24:10 +0300

    Сивова добавила типы

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,21 @@
-export type sum = (a: number, b: number) => number
+export type RuleFunction = (value: string) => string | null;
+
+export interface IFieldChain {
+    string(): IFieldChain;
+    min(minLength: number, errorMessage?: string): IFieldChain;
+    max(maxLength: number, errorMessage?: string): IFieldChain;
+}
+
+export interface IFormValidator {
+    field(fieldName: string): IFieldChain;
+    
+    validate(): ValidationResult;
+    
+}
+
+export interface ValidationResult {
+    success: boolean; 
+    errors: Record<string, string | null>; 
+}
+
+export type FormFactory = (formElement: HTMLFormElement) => IFormValidator;
