fe163df50e7f6b10b97ace7b0ff5c5773c1b85bd
Dasha <lefward@yandex.ru>
2025-12-09 00:24:18 +0300

    начало реализации метода field, мирешкина

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import type { IFormValidator, IFieldChain } from "./types/types";
+
 // Точка входа в ваше решение
-function form(form: HTMLFormElement) {
+function form(form: HTMLFormElement): IFormValidator {
   const inputs = form.querySelectorAll("input");
   if (inputs.length === 0) {
     throw new Error("нет инпутов!!");
@@ -22,4 +24,26 @@ function form(form: HTMLFormElement) {
       throw new Error("нет поля для вывода ошибки");
     }
   }
+
+  return {
+    field(fieldName: string): IFieldChain {
+      const fieldik = form.querySelector(`input[name="${fieldName}"]`);
+      if (!fieldik) {
+        throw new Error("нет такого поля");
+      }
+      const fieldMethods = {
+        string() {
+          return fieldMethods;
+        },
+        min(errorMessage: string) {
+          return fieldMethods;
+        },
+        max(errorMessage: string) {
+          return fieldMethods;
+        },
+      };
+      return fieldMethods;
+    },
+    validate() {},
+  };
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,19 +2,14 @@ export type RuleFunction = (value: string) => string | null;
 
 export interface IFieldChain {
   string(): IFieldChain;
-  min(minLength: number, errorMessage?: string): IFieldChain;
-  max(maxLength: number, errorMessage?: string): IFieldChain;
+  min(errorMessage: string): IFieldChain;
+  max(errorMessage: string): IFieldChain;
 }
 
 export interface IFormValidator {
   field(fieldName: string): IFieldChain;
 
-  validate(): ValidationResult;
-}
-
-export interface ValidationResult {
-  success: boolean;
-  errors: Record<string, string | null>;
+  validate(): void;
 }
 
 export type FormFactory = (formElement: HTMLFormElement) => IFormValidator;
