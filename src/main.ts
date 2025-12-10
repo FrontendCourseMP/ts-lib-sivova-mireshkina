da88acdd26011dff8f61728b74891a2a6bcd3215
sivova mariya <sivovamrv@gmail.com>
2025-12-10 13:11:14 +0300

    сивова начало тестов

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import * as m from "./lib";
-const check = m.form(document.querySelector("form"));
-check.field("name").required("заполните поле").min("должно быть минимум 3");
-check.validate(console.log);
+// const check = m.form(document.querySelector("form"));
+// check.field("name").required("заполните поле").min("должно быть минимум 3");
+// check.validate(console.log);
+
+  const checkA = m.form(document.getElementById('formA'));
+  checkA.field('name').required('fill me').min('min 3');
+  checkA.validate(console.log);    
+
+
+
+  const checkB = m.form(document.getElementById('formB'));
+  checkB.field('name')
+  checkB.validate(console.log)
+
+
